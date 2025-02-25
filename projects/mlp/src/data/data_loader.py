269c366bc53175f9de1ed66714213766eeb60259
Matthew Evans <matthewevans87@gmail.com>
2025-02-25 11:29:27 -0600

    remove unused mlp code

--- projects/mlp/src/data/data_loader.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
-
-def get_data_loader(train: bool) -> torch.utils.data.DataLoader:
-    transform = transforms.ToTensor()
-    dataset = torchvision.datasets.MNIST(root='./datasets', train=train, transform=transform, download=True)
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
-    return data_loader
